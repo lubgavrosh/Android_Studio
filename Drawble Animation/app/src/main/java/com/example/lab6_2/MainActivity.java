52787b10b086cf80180626cadc3608321bdd9532
Liubomyr Havrosh <lubgavrosh@gmail.com>
2026-05-12 20:10:47 +0200

    Drawble Animation

---
+++ Drawble Animation/app/src/main/java/com/example/lab6_2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lab6_2;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ImageView img = findViewById(R.id.imageView);
+        img.setBackgroundResource(R.drawable.cell_animation);
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                frameAnimation.start();
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onClick(View view) {
+    }
+}
