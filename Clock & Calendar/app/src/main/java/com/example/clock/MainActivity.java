fe802c3f5cf058b8fb7c95b66b08712c5ee8c017
Liubomyr Havrosh <lubgavrosh@gmail.com>
2026-05-12 20:18:04 +0200

    Clock&Calendar app

---
+++ Clock & Calendar/app/src/main/java/com/example/clock/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.clock;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+TextView text;
+CalendarView calendar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        text= findViewById(R.id.textView);
+        calendar= findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                text.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onClick(View view) {
+
+    }
+}
